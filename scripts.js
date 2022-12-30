1025c60e698f6f5f6da65c1c70610d354deb78b3
HappyLego214 <anthony21josh@gmail.com>
2022-12-30 13:22:11 +0800

    adds looping function to program

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
+let playerWon = 0;
+let computerWon = 0;
+let tie = 0;
 let regex = {
     rock:/^rock$/i,
     paper:/^paper$/i, 
     scissors:/^scissors$/i
 }
 
+// provides computer game decision
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 100) - 1; 
 
@@ -18,10 +22,9 @@ function getComputerChoice() {
 }
 
 
-
+// provides player the option to choose fighter
 function getPlayerChoice() {
     let playerChoice = prompt("Choose Your Fighter: Rock | Paper | Scissors");
-
     if (regex.rock.test(playerChoice)) {
         playerChoice = "rock";
     } else if (regex.paper.test(playerChoice)) {
@@ -32,51 +35,77 @@ function getPlayerChoice() {
         playerChoice = "error";
     }
 
-        return playerChoice;
-    
+    return playerChoice;    
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
 
-console.log(playerSelection, "|", computerSelection);
+// loops the program 5 times
+for (let i = 0; i < 5; i++)  {
 
+    let playerSelection = getPlayerChoice();
+    let computerSelection = getComputerChoice();
+
+    playRound(playerSelection, computerSelection);
+}
+
+//provides the winning result between computer selection and player selection
 function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection === computerSelection ) {
-        console.log("It is a Tie!");
-    } else if (playerSelection == "rock" ) {
-        
-        if (computerSelection == "paper") {
-        console.log("Computer Won!");
-            } else {
-            console.log("Player Won!");
-            }
+        if (playerSelection === computerSelection ) {
+            console.log("It is a Tie!");
+            tie++
+        } else if (playerSelection == "rock" ) {
+            
+            if (computerSelection == "paper") {
+            computerWon++;
+            console.log("Computer Won!");
+                } else {
+                playerWon++;
+                console.log("Player Won!");
+                }
 
-    } else if (playerSelection == "scissors") {
-        
-        if (computerSelection == "rock") {
-        console.log("Computer Won!");
-            } else {
-            console.log("Player Won!");
-            }
+        } else if (playerSelection == "scissors") {
+            
+            if (computerSelection == "rock") {
+            computerWon++;
+            console.log("Computer Won!");
+                } else {
+                playerWon++;    
+                console.log("Player Won!");
+                }
 
-    } else if (playerSelection == "paper") {
-        
-        if (computerSelection == "scissors") {
-        console.log("Computer Won!");
-            } else {
-            console.log("Player Won!");
-            } 
+        } else if (playerSelection == "paper") {
+            
+            if (computerSelection == "scissors") {
+            computerWon++;    
+            console.log("Computer Won!");
+                } else {
+                playerWon++;
+                console.log("Player Won!");
+                } 
 
-    } else {
-        console.log("Input Error!");
-    }
-
+        } else {
+            console.log("Input Error!");
+        }
 }
 
-playRound(playerSelection, computerSelection);
+
+// provide the overall result from the five games
+function gameResult() {
+    if ((tie > computerWon) && (tie > playerWon)) {
+        console.log("Nobody Won or Lost!");
+    } else if (playerWon > computerWon) {
+        console.log("You Won!")
+    } else {
+        console.log("You Lost!")
+    }
+}
+
+gameResult()
 
 
+// keeps track of the number of results for each 5 rounds
+console.log(playerWon);
+console.log(computerWon);
+console.log(tie);
 
-    
